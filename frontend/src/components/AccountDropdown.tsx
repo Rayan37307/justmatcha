4af6f2e99e79f93e627062edd66e8b5c0c09a0d6
Rayan37307 <r11137307@gmail.com>
2025-08-27 22:40:11 +0600

    dropdown fixed

--- frontend/src/components/AccountDropdown.tsx
+++ frontend/src/components/AccountDropdown.tsx
@@ -7,40 +7,40 @@ import {
 import { Dropdown } from "./base/dropdown/dropdown";
 import useAuthStore from "../store/useAuthStore";
 import { Link, useNavigate } from "react-router-dom";
+import { Button as AriaButton } from "react-aria-components"; // 👈 import it here
 
 export const AccountDropdown = () => {
   const { logout, isAuthenticated } = useAuthStore();
   const navigate = useNavigate();
+
   return (
     <Dropdown.Root>
-      <button className="text-green-800 flex items-center justify-center cursor-pointer">
-        <User01 />
-      </button>
+      {/* ✅ Trigger button */}
+      <AriaButton className="text-green-800 flex items-center justify-center cursor-pointer rounded-md">
+        <User01 className="size-5" />
+      </AriaButton>
 
       <Dropdown.Popover>
         <Dropdown.Menu>
-          {isAuthenticated ?
-          (
+          {isAuthenticated ? (
             <Dropdown.Section>
-            <Dropdown.Item icon={User01}>
-              <Link to="/account">View profile</Link>
-            </Dropdown.Item>
-          </Dropdown.Section>
-          )
-          :
-          (
+              <Dropdown.Item icon={User01}>
+                <Link to="/account">View profile</Link>
+              </Dropdown.Item>
+            </Dropdown.Section>
+          ) : (
             <Dropdown.Section>
-            <Dropdown.Item icon={LogIn02}>
-              <Link to="/login">login</Link>
-            </Dropdown.Item>
-             <Dropdown.Item icon={LogIn01}>
-              <Link to="/signup">Sign up</Link>
-            </Dropdown.Item>
-          </Dropdown.Section>
-          )
-          }
-          
+              <Dropdown.Item icon={LogIn02}>
+                <Link to="/login">Login</Link>
+              </Dropdown.Item>
+              <Dropdown.Item icon={LogIn01}>
+                <Link to="/signup">Sign up</Link>
+              </Dropdown.Item>
+            </Dropdown.Section>
+          )}
+
           <Dropdown.Separator />
+
           <Dropdown.Section>
             {isAuthenticated && (
               <Dropdown.Item
